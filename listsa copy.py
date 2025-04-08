550d3c697223b9fa48315dd10f38d34b7f8bef9d
gersin231 <gersonsouza4839@gmail.com>
2025-04-07 21:24:00 -0400

    Update listsa copy.py

--- listsa copy.py
+++ listsa copy.py
@@ -1,25 +1,32 @@
-"********************************"
-"calculadora"
+print("******************************** \n"
+"calculadora\n"
 
-"1 adição"
-"2 subtração"
-"3 multiplicação"
-"4 divisão"
-"********************************"
-n=int(input("digite um numero: "))
-n2=int(input("digite um numero: "))
+"1 adição\n"
+"2 subtração\n"
+"3 multiplicação\n"
+"4 divisão\n"
+"********************************")
+
 esc=input("escolha: ")
 
 if esc =="1":
+    n=int(input("digite um numero: "))
+    n2=int(input("digite um numero: "))
     res= n+n2
     print(res)
 elif esc =="2":
+    n=int(input("digite um numero: "))
+    n2=int(input("digite um numero: "))
     res= n-n2
     print(res)
 elif esc =="3":
+    n=int(input("digite um numero: "))
+    n2=int(input("digite um numero: "))
     res= n*n2
     print(res)
 elif esc =="4":
+    n=int(input("digite um numero: "))
+    n2=int(input("digite um numero: "))
     res= n/n2
     print(res)
 
