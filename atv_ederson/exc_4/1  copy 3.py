c47dde8b4c7d4455b59b2e4d6283c8c22dfc195d
gersin231 <gersonsouza4839@gmail.com>
2025-04-10 21:49:16 -0400

    atlz

--- atv_ederson/exc_4/1  copy 3.py
+++ atv_ederson/exc_4/1  copy 3.py
@@ -1,11 +1,35 @@
+nome=input("nome: ")
 salario= float(input("Salario : "))
+b=100
+if (salario<280.55):
+    porcent =22.51 
+    a=(salario*(22.51/100))
+    p_aumento=a-b
 
-if (salario<500):
-    print((salario*(15/100)))
-
-elif(salario>=500 and salario<=1000):
-    print((salario*(10/100)))
+elif(salario>=280.56 and salario<=709.72):
+    porcent =15.39 
+    a=(salario*(15.39/100))
+    p_aumento=a-b
     
+elif(salario>709.72 and salario<=1501.33):
+    porcent =10.97 
+    a=(salario*(10.97/100))
+    p_aumento=a-b
+    p_aumento=a-b
+
 else:
-    print((salario*(5/100)))
+     porcent =5.19 
+     a=(salario*(5.19/100))
+     p_aumento=a-b
+    
+print(nome)
+print(f"antes do reajuste:{salario:.2f}")
+print(f"porcentagem do Aumento: {p_aumento:.2f} R$")
+print(f"valor do aumento: {porcent:.2f}%")
+print(f"novo salario: {salario+a:2f} R$4")
+
+
+
+
+
 
--- atv_ederson/exc_4/2.py
+++ atv_ederson/exc_4/2.py
@@ -1,6 +1,74 @@
-a=float(input("Digite o valor: "))
-b= float(input("Digite o valor: "))
-c= float(input("Digite o valor: "))
+# a=float(input("Digite o valor: "))
+# b= float(input("Digite o valor: "))
+# c= float(input("Digite o valor: "))
 
-res= (a+b) *c
-print(res)
+
+# if (a>b and b>c):
+#     print(a,"e maior que", b ,"e",c)
+
+# elif(a>c and c>b):
+#     print(b,"e maior que", a ,"e",c)
+
+# elif(b>a and a>c):
+#     print(c,"e maior que", a ,"e",b)
+
+# elif(b>c and c>a):
+#     print(b,"e menor que", a ,"e",c)
+
+# elif(c>a and c>b):
+#     print(c,"e menor que", a ,"e",b)
+
+
+# elif c > b and b < a:
+#     print("a e c são os iguais:", a)
+
+# else:
+#     print("valores são iguais:")
+# a=input("Digite o produto: ")
+# b= input("Digite o produto: ")
+# c= input("Digite o produto: ")
+
+# a1=float(input("Digite o valor do produto:"))
+# b2= float(input("Digite o valor do produto:"))
+# c3= float(input("Digite o valor do produto:" ))
+
+
+# if (a1<b2 and b2<c3):
+#     print("o preço do",a,"e maior que o do", b ,"e do",c)
+
+
+# elif(b<a and a<c):
+#     print("o preço do",b,"e maior que o do", a ,"e do",c)
+
+
+
+# elif(c<a and c<b):
+#     print("o preço do",c,"e maior que o do", b ,"e do",a)
+
+
+# else:
+#     print("")
+
+a=float(input("Digite o valor do produto:"))
+b= float(input("Digite o valor do produto:"))
+c= float(input("Digite o valor do produto:" ))
+
+
+if (a>b and a>c and b>c):
+    print(a, b ,c)
+elif(a>b and a>c and c>b):
+    print(a, c ,b)
+
+elif(b>a and b>c and a>c):
+    print(b, a ,c)
+
+elif(b>a and b>c and c>a):
+    print(b, c ,b)
+
+elif(c>b and c>a and a>b):
+    print(c, b ,a)
+
+elif(c>b and c>a and b>a):
+    print(c, a ,b)
+else:
+    print("")
