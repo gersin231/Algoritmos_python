3f52af7ad8552f0fcc183702d549e8a96426685e
gersin231 <gersonsouza4839@gmail.com>
2025-04-15 21:49:36 -0400

    Update 3.py

--- atv_ederson/exc_4/3.py
+++ atv_ederson/exc_4/3.py
@@ -1,10 +1,13 @@
-nome=input("Digite o nome: ")
-idade=input("Digite a idade: ")
-sexo=input("Digite o sexo : ")
+# nome=input("Digite o nome: ")
+# idade=input("Digite a idade: ")
+# sexo=input("Digite o sexo : ")
 
-a=float(input("digite a nota 1: "))
-b=float(input("digite a nota 2: "))
+a=float(input("digite a n: "))
+b=float(input("digite a a1: "))
+c=float(input("digite r: "))
 
-media= (a+b)/2
+an= b+((a*c)-(1*c))
+s=((b*a+an*a)/2)+42
 
-print("media; ",media)
+print("an: ",an)
+print("soma: ",s)
