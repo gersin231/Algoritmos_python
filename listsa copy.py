25b5f3199974f642b77899916d82009806dd148a
gersin231 <gersonsouza4839@gmail.com>
2025-04-24 20:30:37 -0400

    Update listsa copy.py

--- listsa copy.py
+++ listsa copy.py
@@ -11,27 +11,27 @@ while True:
     esc=input("escolha: ")
 
     if esc =="1":
-        print("adição\n")
-        n=int(input("digite um numero: "))
-        n2=int(input("digite um numero: "))
+        print("adição")
+        n=float(input("digite um numero: "))
+        n2=float(input("digite um numero: "))
         res= n+n2
         print(res)
     elif esc =="2":
-        print("subtração\n")
-        n=int(input("digite um numero: "))
-        n2=int(input("digite um numero: "))
+        print("subtração")
+        n=float(input("digite um numero: "))
+        n2=float(input("digite um numero: "))
         res= n-n2
         print(res)
     elif esc =="3":
-        print("multiplicação\n")
-        n=int(input("digite um numero: "))
-        n2=int(input("digite um numero: "))
+        print("multiplicação")
+        n=float(input("digite um numero: "))
+        n2=float(input("digite um numero: "))
         res= n*n2
         print(res)
     elif esc =="4":
-        print("divisa\n")
-        n=int(input("digite um numero: "))
-        n2=int(input("digite um numero: "))
+        print("divisa")
+        n=float(input("digite um numero: "))
+        n2=float(input("digite um numero: "))
         res= n/n2
         print(res)
 
